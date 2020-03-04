185e0ba04a8a0cb9a90b69da3f50ec25ada51c55
Keoni Mahelona <keoni@kmahelona.net>
2020-03-04 22:31:34 +1300

    fix merge stuff

--- python_apps/airtime_analyzer/setup.py
+++ python_apps/airtime_analyzer/setup.py
@@ -29,11 +29,7 @@ setup(name='airtime_analyzer',
       packages=['airtime_analyzer'],
       scripts=['bin/airtime_analyzer'],
       install_requires=[
-<<<<<<< HEAD
-          'mutagen>=1.43.0', # got rid of specific version requirement 
-=======
           'mutagen~=1.43',
->>>>>>> python3
           'pika~=1.1.0',
           'file-magic',
           'nose',
