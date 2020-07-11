5dcc5ae40d0b9f3a75a350aa7f8c03145c82be55
Keoni Mahelona <keoni@kmahelona.net>
2020-07-11 16:06:35 +1200

    Update to kilobytes not bytes

--- python_apps/libretime_watch/libretime_watch/log.py
+++ python_apps/libretime_watch/libretime_watch/log.py
@@ -3,7 +3,7 @@ import logging
 import logging.handlers
 
 def setup(log_file, level=logging.INFO):
-    log_handler = logging.handlers.RotatingFileHandler(log_file, mode='a', maxBytes=512, backupCount=5)
+    log_handler = logging.handlers.RotatingFileHandler(log_file, mode='a', maxBytes=1024*500, backupCount=5)
     formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s')
     log_handler.setFormatter(formatter)
     logger = logging.getLogger()
