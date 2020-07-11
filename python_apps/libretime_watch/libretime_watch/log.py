47816c513dac32692c2985019671f69fdddb7beb
Keoni Mahelona <keoni@kmahelona.net>
2020-07-11 16:20:38 +1200

    Fine tune log for watch folder

--- python_apps/libretime_watch/libretime_watch/log.py
+++ python_apps/libretime_watch/libretime_watch/log.py
@@ -2,8 +2,8 @@
 import logging
 import logging.handlers
 
-def setup(log_file, level=logging.INFO):
-    log_handler = logging.handlers.RotatingFileHandler(log_file, mode='a', maxBytes=1024*500, backupCount=5)
+def setup(log_file, level=logging.WARNING, count=1):
+    log_handler = logging.handlers.RotatingFileHandler(log_file, mode='a', maxBytes=1024*500, backupCount=count)
     formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s')
     log_handler.setFormatter(formatter)
     logger = logging.getLogger()
